103fa7930ed634d38a6eddd57713191f06763605
JaySant <jaysant15@gmail.com>
2023-04-13 13:55:09 -0300

    requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,8 +1,20 @@
 import sys
+from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    "Não há implementação"
+    data = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': len(txt_importer(path_file)),
+            'linhas_do_arquivo': txt_importer(path_file),
+        }
+
+    for file in range(0, len(instance)):
+        if path_file not in [instance.search(file)["nome_do_arquivo"] ==
+                             data['nome_do_arquivo']]:
+            return
+    instance.enqueue(data)
+    print(data, file=sys.stdout)
 
 
 def remove(instance):
@@ -11,12 +23,12 @@ def remove(instance):
 
     else:
         file = instance.dequeue()
-        print(f"Arquivo {file['arquivo']} removido com sucesso")
+        print(f'Arquivo {file["nome_do_arquivo"]} removido com sucesso')
 
 
 def file_metadata(instance, position):
     try:
         search = instance.search(position)
-        return search
+        return print(search)
     except IndexError:
-        sys.stderr.write('Posição inválida\n')
+        sys.stderr.write("Posição inválida\n")
