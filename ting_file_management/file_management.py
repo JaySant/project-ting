586a15f1240d4f122855493478b1d1a150aab8a5
JaySant <jaysant15@gmail.com>
2023-04-13 12:04:08 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    
+    try:
+        if not path_file.endswith(".txt"):
+            sys.stderr.write('Formato inválido')
+            return []
+        with open(path_file, 'r') as file:
+            return file.read().split('\n')
+
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
