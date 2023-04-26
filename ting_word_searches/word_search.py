1b56abb01cdaf00d554f43245d4c5df7cfaed191
JaySant <jaysant15@gmail.com>
2023-04-25 22:34:11 -0300

    requisito 7 e 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,34 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list_result = []
+    queue_copy = instance.search(0)["linhas_do_arquivo"]
+    for index in range(len(instance)):
+        file = instance.search(index)
+        occurrences = []
+        for i, line in enumerate(queue_copy):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": i+1})
+        if len(occurrences) > 0:
+            list_result.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+    return list_result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    list_result = []
+    queue_copy = instance.search(0)["linhas_do_arquivo"]
+    for index in range(len(instance)):
+        file = instance.search(index)
+        occurrences = []
+        for i, line in enumerate(queue_copy):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": i+1, "conteudo": line.strip()})
+        if len(occurrences) > 0:
+            list_result.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+    return list_result
